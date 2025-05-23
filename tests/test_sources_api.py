59a5113f1bd2fd757b86ef82d9f23c87984ec45d
Ільницький Максим <ilnickijmaksim4@gmail.com>
2025-05-23 09:55:19 +0300

    7

--- tests/test_sources_api.py
+++ tests/test_sources_api.py
@@ -1,18 +1,18 @@
 from fastapi.testclient import TestClient
-from backend.app import app, news_store
+from backend.app import app, news_store, store
 from backend.config import STUDENT_ID
 
 client = TestClient(app)
 
 def test_get_empty_sources():
     # Обов'язково очистити список перед тестом!
-    news_store[STUDENT_ID] = []
+    store[STUDENT_ID] = []
     res = client.get(f"/sources/{STUDENT_ID}")
     assert res.status_code == 200
     assert res.json() == {"sources": []}
 
 def test_add_and_get_source():
-    news_store[STUDENT_ID] = []
+    store[STUDENT_ID] = []
     res1 = client.post(f"/sources/{STUDENT_ID}", json={"url": "<https://example.com/rss>"})
     assert res1.status_code == 200
     assert "<https://example.com/rss>" in res1.json()["sources"]
